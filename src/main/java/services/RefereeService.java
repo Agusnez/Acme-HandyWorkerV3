aa3b41c7b83c57013be712e38253c29ada8453c8
Agustín Núñez <seviagus@gmail.com>
2018-11-20 00:17:27 +0100

    Completa Simple CRUD servicios de Agustín

--- src/main/java/services/RefereeService.java
+++ src/main/java/services/RefereeService.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.RefereeRepository;
 import domain.Referee;
@@ -13,42 +14,50 @@ import domain.Referee;
 @Transactional
 public class RefereeService {
 	// Managed repository -------------------------------------------
-	
-			@Autowired
-			private RefereeRepository refereeRepository;
-			
-			// Supporting services ------------------------------------------
-			
-			// Simple CRUD methods ------------------------------------------
-			
-			public Referee create() {
 
-				return null;
+	@Autowired
+	private RefereeRepository refereeRepository;
 
-			}
+	// Supporting services ------------------------------------------
 
-			public Collection<Referee> findAll() {
+	// Simple CRUD methods ------------------------------------------
 
-				return null;
+	public Referee create() {
 
-			}
+		return new Referee();
 
-			public Referee findOne(final int refereeId) {
+	}
 
-				return null;
+	public Collection<Referee> findAll() {
+		Assert.notNull(this.refereeRepository);
+		Collection<Referee> result = this.refereeRepository.findAll();
+		Assert.notNull(result);
+		return result;
 
-			}
+	}
 
-			public Referee save(final Referee referee) {
+	public Referee findOne(final int refereeId) {
+		Assert.isTrue(refereeId != 0);
+		Referee result = this.refereeRepository.findOne(refereeId);
+		Assert.notNull(result);
+		return result;
 
-				return null;
+	}
 
-			}
+	public Referee save(final Referee referee) {
+		Assert.notNull(referee);
 
-			public void delete(final Referee referee) {
+		return this.refereeRepository.save(referee);
 
-			}
-			
-			// Other business methods -----------------------------------------
+	}
+
+	public void delete(final Referee referee) {
+		Assert.notNull(referee);
+		Assert.isTrue(referee.getId() != 0);
+		Assert.isTrue(this.refereeRepository.exists(referee.getId()));
+		this.refereeRepository.delete(referee);
+	}
+
+	// Other business methods -----------------------------------------
 
 }
