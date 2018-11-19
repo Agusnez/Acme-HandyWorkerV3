89a4be62872d24842e0493aef06a8c6af16aed36
Angel C <angcallla@alum.us.es>
2018-11-19 14:05:24 +0100

    1 versión category RyS

--- src/main/java/repositories/CategoryRepository.java
+++ src/main/java/repositories/CategoryRepository.java
@@ -1,9 +1,15 @@
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
+	
+	@Query("select c from Category c where c.parent.id = ?1")
+	Collection<Category> findChildren(int categoryId);
 
 }
--- src/main/java/services/CategoryService.java
+++ src/main/java/services/CategoryService.java
@@ -6,6 +6,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import repositories.CategoryRepository;
 import domain.Category;
@@ -15,35 +16,75 @@ import domain.Category;
 public class CategoryService {
 
 	// Managed Repository ------------------------
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	// Suporting services ------------------------
-	
+
 	// Simple CRUD methods -----------------------
-	
-	public Category create(){
-		return null;
+
+	public Category create() {
+		Category c = new Category();
+
+		return c;
 	}
-	
-	public Collection<Category> findAll(){
-		return null;
+
+	/* no sé si es necesario este método porque no nos aporta nada en principio */
+	public Collection<Category> findAll() {
+		Collection<Category> res;
+
+		/* he visto este Assert en Acme-Certificacion (AnnouncementService) */
+		Assert.notNull(this.categoryRepository);
+		res = categoryRepository.findAll();
+		Assert.notNull(res);
+
+		return res;
+
 	}
-	
-	public Category findOne(int categoryId){
-		return null;
+
+	public Category findOne(int categoryId) {
+		Category c;
+
+		c = categoryRepository.findOne(categoryId);
+		Assert.notNull(c);
+
+		return c;
 	}
-	
-	public Category save(Category category){
-		return null;
+
+	public Category save(Category category) {
+		Category c;
+
+		/* compruebo que sea único */
+		Assert.isTrue(!categoryRepository.exists(category.getId()));
+		c = categoryRepository.save(category);
+
+		return c;
 	}
-	
-	public void delete(Category category){
-		
+
+	public void delete(Category category) {
+		/* compruebo que exista */
+		Assert.isTrue(categoryRepository.exists(category.getId()));
+
+		/* compruebo que no tenga hijos (hojas) */
+		Collection<Category> children = categoryRepository
+				.findChildren(category.getId());
+		Assert.isTrue(children.isEmpty());
+
+		categoryRepository.delete(category);
+
 	}
-	
+
 	// Other business methods -----------------------
-	
-	
+
+	/* me hace falta para el delete y considero además que es necesario */
+	public Collection<Category> findChildren(int categoryId) {
+		Collection<Category> res;
+
+		res = categoryRepository.findChildren(categoryId);
+		Assert.notNull(res);
+
+		return res;
+	}
+
 }
