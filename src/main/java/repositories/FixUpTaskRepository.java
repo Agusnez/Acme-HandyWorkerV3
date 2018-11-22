382d35aa6f3bd5745ccbd56f21f3f1efbc1c8dc1
Souto <jasvbetico@hotmail.com>
2018-11-22 17:23:40 +0100

    arreglos repositorios

--- src/main/java/repositories/FixUpTaskRepository.java
+++ src/main/java/repositories/FixUpTaskRepository.java
@@ -1,7 +1,10 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.FixUpTask;
@@ -9,4 +12,15 @@ import domain.FixUpTask;
 @Repository
 public interface FixUpTaskRepository extends JpaRepository<FixUpTask, Integer> {
 
+	@Query("select avg(f.applications.size), min(f.applications.size), max(f.applications.size), stddev(f.applications.size) from FixUpTask f")
+	Collection<Double> statsOfApplicationsPerFixUpTask();
+
+	@Query("select avg(f.maximumPrice.amount), min(f.maximumPrice.amount), max(f.maximumPrice.amount), stddev(f.maximumPrice.amount) from FixUpTask f")
+	Collection<Double> statsOfMaximumPricePerFixUpTask();
+
+	@Query("select avg(f.complaints.size), min(f.complaints.size), max(f.complaints.size), stddev(f.complaints.size) from FixUpTask f")
+	Collection<Double> statsOfComplaintsPerFixUpTask();
+
+	@Query("select (select count(f1) from FixUpTask f1 where f1.complaints.size > 0)/count(f2)*1.0 from FixUpTask f2")
+	Double ratioOfFixUpTasksWithComplaint();
 }
--- src/main/java/repositories/TutorialRepository.java
+++ src/main/java/repositories/TutorialRepository.java
@@ -1,9 +1,12 @@
+
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import domain.Tutorial;
 
-public interface TutorialRepository extends JpaRepository<Tutorial, Integer>{
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Integer> {
 
 }
