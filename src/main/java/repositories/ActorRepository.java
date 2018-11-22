f36d346de464184a9695587c334f33e55632f82f
albriozea <36491509+albriozea@users.noreply.github.com>
2018-11-22 11:54:25 +0100

    ActorRepository

--- src/main/java/repositories/ActorRepository.java
+++ src/main/java/repositories/ActorRepository.java
@@ -1,12 +1,21 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Actor;
 
 @Repository
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
+
+	@Query("select a from Actor a where a.userAccount.id = ?1")
+	Actor findByUserAccountId(int userAccountId);
+
+	@Query("select a from Actor a where a.suspicious =true")
+	Collection<Actor> suspiciousActors();
 
 }
