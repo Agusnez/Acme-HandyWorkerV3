916c293c05b06fbda41d34cc7be78ae757712494
raulsfcp4 <iissijara17@gmail.com>
2018-11-28 15:52:09 +0100

    categoryTest

--- src/test/java/services/CategoryServiceTest.java
+++ src/test/java/services/CategoryServiceTest.java
@@ -1,7 +1,6 @@
 
 package services;
 
-
 import java.util.Collection;
 
 import org.junit.Test;
@@ -25,182 +24,154 @@ public class CategoryServiceTest extends AbstractTest {
 
 	//Service under test ------------------------------------------
 	@Autowired
-	private CategoryService categoryService;
-	
+	private CategoryService		categoryService;
+
 	@Autowired
-	private FixUpTaskService fixUpTaskService;
+	private FixUpTaskService	fixUpTaskService;
+
 
 	//Tests -------------------------------------------------------
-	
-	
-		// Simple CRUD methods -----------------------
+
+	// Simple CRUD methods -----------------------
 	@Test
-	public void testCreateCategory(){
+	public void testCreateCategory() {
 		super.authenticate("admin");
-		
-		Category c; 
+
+		Category c;
 		c = this.categoryService.create();
 		Assert.notNull(c.getParent());
-		
+
 		super.authenticate(null);
 	}
-	
+
 	@Test
-	public void testSaveCategory(){
+	public void testSaveCategory() {
 		super.authenticate("admin");
-		
-		Category c, saved; 
+
+		Category c, saved;
 		c = this.categoryService.create();
-		c.setName("zapatos"); /*NO POPULAR ESTA CATEGORY*/
-		
+		c.setName("zapatos"); /* NO POPULAR ESTA CATEGORY */
+
 		saved = this.categoryService.save(c);
-		
-		Collection<Category> categories = this.categoryService.findAll();
+
+		final Collection<Category> categories = this.categoryService.findAll();
 		Assert.isTrue(categories.contains(saved));
-		
+
 		super.authenticate(null);
-		
+
 	}
-	
-	
+
 	@Test
-	public void testSaveDuplicate(){
+	public void testSaveDuplicate() {
 		super.authenticate("admin");
-		
-		Category c1, c2, saved1; 
-		
+
+		Category c1, c2, saved1;
+
 		c1 = this.categoryService.create();
-		c1.setName("zapatos"); /*NO POPULAR ESTA CATEGORY*/
-		
+		c1.setName("zapatos"); /* NO POPULAR ESTA CATEGORY */
+
 		c2 = this.categoryService.create();
 		c2.setName("zapatos");
-		
+
 		saved1 = this.categoryService.save(c1);
-		
-		Collection<Category> categories = this.categoryService.findAll();
-		
+
+		final Collection<Category> categories = this.categoryService.findAll();
+
 		Assert.isTrue(categories.contains(saved1));
-		
-		
-		try{
+
+		try {
 			this.categoryService.save(c2);
-		}catch (Exception e){
-			
+		} catch (final Exception e) {
+
 		}
-		Collection<Category> categories2 = this.categoryService.findAll();
-		
-		for(Category c : categories2){
-			Assert.isTrue(!(c.getName()==saved1.getName() && c.getId()!= saved1.getId()));
-		}
-		
-		
+		final Collection<Category> categories2 = this.categoryService.findAll();
+
+		for (final Category c : categories2)
+			Assert.isTrue(!(c.getName() == saved1.getName() && c.getId() != saved1.getId()));
+
 		super.authenticate(null);
-		
+
 	}
-	
+
 	@Test
-	public void testDeleteCategory(){
+	public void testDeleteCategory() {
 		super.authenticate("admin");
-		
-		Category c, saved; 
+
+		Category c, saved;
 		c = this.categoryService.create();
-		c.setName("zapatos"); /*NO POPULAR ESTA CATEGORY*/
-		
+		c.setName("zapatos"); /* NO POPULAR ESTA CATEGORY */
+
 		saved = this.categoryService.save(c);
-		
+
 		this.categoryService.delete(saved);
-		
-		
+
 		super.authenticate(null);
-		
+
 	}
-	
+
 	@Test
-	public void testDeleteCATEGORY(){
+	public void testDeleteCATEGORY() {
 		super.authenticate("admin");
 		Category c;
-		
+
 		c = this.categoryService.findByName("CATEGORY");
-		
-		try{
-			
+
+		try {
+
 			this.categoryService.delete(c);
-			
-		}catch (Exception e){
-			
+
+		} catch (final Exception e) {
+
 		}
-		
-		Collection<Category> categories = this.categoryService.findAll();
+
+		final Collection<Category> categories = this.categoryService.findAll();
 		Assert.isTrue(categories.contains(c));
-				
+
 		super.authenticate(null);
-		
+
 	}
-	
-	
+
 	@Test
-	public void testDeleteCategoryUnlogged(){ /*Con este test vamos a comprobar la seguridad de autenticación*/
-		super.authenticate("customer1"); /*para poder acceder al findOne*/
-		
-		Category category2 = this.categoryService.findOne(2594); /*Category 2 en el populate*/
-		
+	public void testDeleteCategoryUnlogged() { /* Con este test vamos a comprobar la seguridad de autenticación */
+		super.authenticate("customer1"); /* para poder acceder al findOne */
+
+		final Category category2 = this.categoryService.findOne(2594); /* Category 2 en el populate */
+
 		super.authenticate(null);
-		/*ahora estando deslogeado*/
-		
-		try{
+		/* ahora estando deslogeado */
+
+		try {
 			this.categoryService.delete(category2);
-		}catch (Exception e){
-			
+		} catch (final Exception e) {
+
 		}
-		
-		/*un personaje se logea y busca nuestra category víctima*/
+
+		/* un personaje se logea y busca nuestra category víctima */
 		super.authenticate("admin");
-		Category categoryFind = this.categoryService.findOne(2594);
-		
+		final Category categoryFind = this.categoryService.findOne(2594);
+
 		Assert.notNull(categoryFind);
-		
+
 		super.authenticate(null);
-		
+
 	}
-	
-//	/*TODO: Da error, los id's son de la BBDD y se corresponden bien...*/
-//	@Test
-//	public void testDeleteCategoryWithChildrenAndFixUpAssociated(){
-//		super.authenticate("admin");
-//		
-//		Category category2 = this.categoryService.findOne(2594);/*categoria padre*/
-//		Category category4 = this.categoryService.findOne(2596); /*categoría hija*/
-//		FixUpTask fixUpTask = this.fixUpTaskService.findOne(2600); /*FixUp que tiene como categoría la categor2*/
-//		
-//		this.categoryService.delete(category2);
-//		
-//		Assert.isTrue(category4.getParent().equals(category2.getParent()));
-//		Assert.isTrue(fixUpTask.getCategory().equals(category2.getParent()));
-//
-//		
-//		super.authenticate(null);
-//		
-//	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	/* TODO: Da error, los id's son de la BBDD y se corresponden bien... */
+	@Test
+	public void testDeleteCategoryWithChildrenAndFixUpAssociated() {
+		super.authenticate("admin");
 
+		final Category category2 = this.categoryService.findOne(super.getEntityId("category2"));/* categoria padre */
+		final Category category4 = this.categoryService.findOne(super.getEntityId("category4")); /* categoría hija */
+		final FixUpTask fixUpTask = this.fixUpTaskService.findOne(super.getEntityId("fixUpTask1")); /* FixUp que tiene como categoría la categor2 */
+
+		this.categoryService.delete(category2);
+
+		Assert.isTrue(category4.getParent().equals(category2.getParent()));
+		Assert.isTrue(fixUpTask.getCategory().equals(category2.getParent()));
+
+		super.authenticate(null);
+
+	}
 
 }
