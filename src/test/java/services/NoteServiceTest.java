021a00ad62b9d5aa33ef649903c44282410c40f2
raulsfcp4 <iissijara17@gmail.com>
2018-11-28 18:34:52 +0100

    cambio note necesario

--- src/test/java/services/NoteServiceTest.java
+++ src/test/java/services/NoteServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
@@ -18,6 +19,7 @@ import domain.Note;
 @ContextConfiguration(locations = {
 	"classpath:spring/datasource.xml", "classpath:spring/config/packages.xml"
 })
+@Transactional
 public class NoteServiceTest extends AbstractTest {
 
 	//Service under test ------------------------------------------
