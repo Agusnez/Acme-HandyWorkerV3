6f34772f15933158ab482d3c90d8d029909ebe03
albriozea <36491509+albriozea@users.noreply.github.com>
2018-11-28 11:32:34 +0100

    configurationText

--- src/test/java/services/ConfigurationServiceTest.java
+++ src/test/java/services/ConfigurationServiceTest.java
@@ -1,14 +1,18 @@
 
 package services;
 
+import java.util.Collection;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
+import domain.Configuration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -19,10 +23,23 @@ public class ConfigurationServiceTest extends AbstractTest {
 
 	//Service neder test ------------------------------------------
 
+	@Autowired
+	private ConfigurationService	configurationService;
+
+
 	//Tests -------------------------------------------------------
-	// TODO Configuration testing
+
 	@Test
-	public void testing() {
-		Assert.isTrue(1 == new Integer(1));
+	public void testSave() {
+		this.authenticate("admin");
+
+		final Configuration c = this.configurationService.findOne(super.getEntityId("configuration"));
+
+		c.setVatTax(0.18);
+		final Configuration saved = this.configurationService.save(c);
+
+		final Collection<Configuration> config = this.configurationService.findAll();
+		Assert.isTrue(config.contains(saved), "----- Fallo metodo save -----");
+
 	}
 }
